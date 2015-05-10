8b541f9a64df7c76ebe3134e52193c4e253edda3
RodrigoGonzalez <rodrigo.gonzalez@colorado.edu>
2015-05-10 15:19:02 -0600

    updated R code for plot 1 and included plot 1

--- plot1.R
+++ plot1.R
@@ -1,2 +1,7 @@
 ##The following loads the necessary data into the workspace
+setwd("~/Documents/DATASETS")
+hps <- file("household_power_consumption.txt")
+data <- read.table(text = grep("^[1,2]/2/2007", readLines(hps), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
 
+# Generating Plot 1
+hist(data$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
