1f5b73f5b85ab053a70c22fe021d5b9be919bc33
Rodrigo Gonzalez Erazo <rodrigo.gonzalez@colorado.edu>
2016-04-03 22:48:01 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -11,7 +11,7 @@ data <- read.table(text = grep("^[1,2]/2/2007", readLines(hps), value = TRUE),
                    col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
                    sep = ";", header = TRUE)
 
-## The following generates plot 1
+## The following generates Plot 1
 
 hist(data$Global_active_power, col = "red", main = paste("Global Active Power"), 
      xlab = "Global Active Power (kilowatts)")
